2a350b5ab7539618b5ba113780721042fbc4cff0
wallaceroque <roque.wallace@gmail.com>
2012-09-01 20:18:23 -0300

    Controller do subscribe

---
+++ src/main/java/br/com/parrot/controller/SubscribeController.java
@@ -0,0 +1,45 @@
+package br.com.parrot.controller;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.view.Results;
+import br.com.parrot.github.repository.Subscribe;
+
+@Resource
+@Path("/subscribe")
+public class SubscribeController {
+	
+	private final Result result;
+	private final Subscribe subscribe;
+	
+	
+	public SubscribeController(Result result, Subscribe subscribe){
+		this.result = result;
+		this.subscribe = subscribe;
+	}
+
+	@Get("/")
+	public void redirectToRegistra(String email) throws ClientProtocolException, JSONException, IOException, URISyntaxException {
+		this.subscribe.writeEmailOnTxt(email);
+		result.redirectTo(this).registra(email);
+		
+	}
+	
+	@Post("/")
+	public void registra(String email) throws ClientProtocolException, JSONException, IOException, URISyntaxException{
+		System.out.println(email);
+		this.subscribe.writeEmailOnTxt(email);
+		result.include("mensagem", "Seu email foi registrado com sucesso!");
+		result.use(Results.page()).forwardTo("/index.jsp");
+	}
+	
+}
--- src/main/java/br/com/parrot/github/repository/Subscribe.java
+++ src/main/java/br/com/parrot/github/repository/Subscribe.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import br.com.caelum.vraptor.ioc.Component;
+
+@Component
 public class Subscribe {
 
 	/**
@@ -17,9 +20,11 @@ public class Subscribe {
 
 	public void writeEmailOnTxt(String email){
 		try{
-			String path = this.getClass().getResource("/email.txt").getPath();
-			File file = new File(path);
-			FileWriter fw = new FileWriter(file);
+			//String path = this.getClass().getResource("/email.txt").getPath();
+			String path = "//home//parrot//";
+			System.out.println(path);
+			File file = new File(path + "email.txt");
+			FileWriter fw = new FileWriter(file,true);
 			fw.write(email + ";");
 			fw.close();
 		}catch (IOException e ){
