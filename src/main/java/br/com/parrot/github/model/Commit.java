cf00a4c49f93e238900bb59bbc3ed91c9b2ba2e9
jeancsouza <jcsouza.first@gmail.com>
2012-09-01 06:44:26 -0300

    esboço da comunicação com pronta com o git hub.

---
+++ src/main/java/br/com/parrot/github/model/Commit.java
@@ -0,0 +1,61 @@
+package br.com.parrot.github.model;
+
+import java.io.Serializable;
+
+public class Commit implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7693389980006477643L;
+
+	private String author;
+	private String sha;
+	private String distinct;
+	private String message;
+	private String url;
+	
+	public Commit(){
+		super();
+	}
+	
+	public Commit(String author, String message, String url) {
+		super();
+		this.author = author;
+		this.message = message;
+		this.url = url;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getSha() {
+		return sha;
+	}
+	public void setSha(String sha) {
+		this.sha = sha;
+	}
+	public String getDistinct() {
+		return distinct;
+	}
+	public void setDistinct(String distinct) {
+		this.distinct = distinct;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+}
