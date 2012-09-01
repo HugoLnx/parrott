bd62439b07e1c0fb90ca8653fa26b8194ea22e09
wallaceroque <roque.wallace@gmail.com>
2012-09-01 06:52:24 -0300

    Limpeza dos js e adicionando o bootstrap do twitter

--- WebContent/js/mascarasPagador.js
+++
@@ -1,6 +0,0 @@
-(function(Mascara) {
-  Mascara.adicionar("representanteFormBean.cpf", Mascara.Cpf);
-  Mascara.adicionar("representanteFormBean.endereco.cep", Mascara.Cep);
-  Mascara.adicionar("representanteFormBean.telefone", Mascara.Telefone);
-  Mascara.adicionar("representanteFormBean.endereco.numero", Mascara.Integer);
-}(Caelum.PagPag.Mascara));
