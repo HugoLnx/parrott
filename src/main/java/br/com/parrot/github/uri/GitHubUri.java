9f6400b894d34aeddc83ad55a310265e39fb0ea3
Hugo Roque <hugolnx@gmail.com>
2012-09-02 02:36:39 -0300

    uri dos que estão sendo seguidos

--- src/main/java/br/com/parrot/github/uri/GitHubUri.java
+++ src/main/java/br/com/parrot/github/uri/GitHubUri.java
@@ -10,25 +10,34 @@ import br.com.caelum.vraptor.ioc.Component;
 @Component
 public class GitHubUri {
 	private static final String PROTOCOL = "http"; // https
-	private static final String EVENTS_URI = "/users/{username}/events/public";
+	private static final String EVENTS_PATH = "/users/{username}/events/public";
+	private static final String FOLLOWING_PATH = "/users/{username}/following";
 	private static final String HOST = "localhost"; // api.github.com
 	private static final int PORT = 8081; // 403
 
 	public URI publicEvents(String user) throws URISyntaxException {
-		URIBuilder builder = getUriBuilder();
-		builder.setPath(getEventsPathFor(user));
-		builder.setPort(PORT);
-		return builder.build();
+		String path = EVENTS_PATH.replace("{username}", user);
+		return uriWithPath(path);
+	}
+	
+	public URI following(String user) throws URISyntaxException {
+		String path = FOLLOWING_PATH.replace("{username}", user);
+		return uriWithPath(path);
 	}
 
-	private String getEventsPathFor(String username) {
-		return EVENTS_URI.replace("{username}", username);
+	private URI uriWithPath(String path) throws URISyntaxException {
+		return getUriBuilder()
+				.setPath(path)
+				.build();
 	}
 
 	private URIBuilder getUriBuilder() {
 		URIBuilder builder = new URIBuilder();
-		builder.setScheme(PROTOCOL).setHost(HOST);
+		builder.setScheme(PROTOCOL)
+			.setHost(HOST)
+			.setPort(PORT);
 		return builder;
 	}
+
 	
 }
---
+++ src/test/java/br/com/parrot/github/uri/GitHubUriTest.java
@@ -0,0 +1,18 @@
+package br.com.parrot.github.uri;
+
+import static org.junit.Assert.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.Test;
+
+public class GitHubUriTest {
+
+	@Test
+	public void getUriForAnUserFollowingJson() throws URISyntaxException {
+		URI uri = new GitHubUri().following("ashton");
+		assertEquals("http://localhost:8081/users/ashton/following", uri.toString());
+	}
+
+}
